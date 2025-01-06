928e86c66591df547e5ab89a4e9dbf934fa4da23
Emanuele Favero <emafaux89@gmail.com>
2025-01-06 04:57:32 +0100

    add data and index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql2')
+
+// Create a connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  // password: 'password',
+  database: 'test_db',
+})
+
+// Connect
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack)
+    return
+  }
+  console.log('Connected as id ' + connection.threadId)
+})
+
+// Select all rows from the table
+connection.query('SELECT * FROM test_table', (err, results) => {
+  if (err) throw err
+
+  console.log(results)
+})
+
+// Query
+const query = `
+  SELECT name FROM users
+  WHERE age < 20
+  ORDER BY name
+`
+
+connection.query(query, (err, results) => {
+  if (err) throw err
+
+  console.log('Query results:', results)
+})
+
+// Close the connection
+connection.end((err) => {
+  if (err) {
+    console.error('Error closing connection: ' + err.stack)
+    return
+  }
+  console.log('Connection closed')
+})
