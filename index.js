b6dcccd20ec72bc6597115160f93286f6ac7b1a1
Emanuele Favero <emafaux89@gmail.com>
2025-03-26 09:27:18 +0100

    add inner join query

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // * Query
 let query = `
-  SELECT pts FROM playoffs LIMIT 1
+  SELECT players.full_name, playoffs.gp, playoffs.pts, playoffs.fg3_pct FROM players
+  INNER JOIN playoffs ON players.id = playoffs.player_id
+  WHERE playoffs.pts > 1000 AND playoffs.fg3a > 50
+  ORDER BY playoffs.fg3_pct DESC
+  LIMIT 20
 `
 
 // ----------------------------
